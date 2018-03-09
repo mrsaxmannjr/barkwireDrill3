6819ddbe0210591985ef0b4c20cd12f3f42dcf46
James Mann <mrsaxmannjr@gmail.com>
2018-03-09 11:48:53 -0700

    Update add-dog.js

--- drills/version-3/app/public/scripts/add-dog.js
+++ drills/version-3/app/public/scripts/add-dog.js
@@ -1,28 +1,29 @@
 console.log("It worked!");
 document.querySelector("form").addEventListener("submit", addDogFunction);
 
-function addDogFunction(event){
-	event.preventDefault();
-	fetch("/api/dogs", {
-		method: "POST",
-		headers: new Headers({
-			"Content-Type": "application/json"
-		}),
-		body: JSON.stringify(getFormData(event.target))
-	})
-	.then(() => {redirectToDogList();
-	});
+function addDogFunction(event) {
+  event.preventDefault();
+  fetch("/api/dogs", {
+    method: "POST",
+    headers: new Headers({
+      "Content-Type": "application/json",
+    }),
+    body: JSON.stringify(getFormData(event.target)),
+  })
+    .then(() => {
+redirectToDogList();
+    });
 }
 
 function getFormData(form) {
-    var data = new FormData(form);
-	return {
-		name: data.get("name"),
-		profilePicture: data.get("profile-picture"),
-		bio: data.get("bio")
-	};
+  let data = new FormData(form);
+  return {
+    name: data.get("name"),
+    profilePicture: data.get("profile-picture"),
+    bio: data.get("bio"),
+  };
 }
 
-function redirectToDogList(){
-	window.location.assign("/dogs");
+function redirectToDogList() {
+  window.location.assign("/dogs");
 }
